43e10a41828165e4189acc0e907252f20817d4ef
elton-emmanuel <33668494+elton-emmanuel@users.noreply.github.com>
2019-02-20 14:57:41 -0500

    Created a Test  Script

---
+++ test.py
@@ -0,0 +1,5 @@
+from array import *
+LoginTemp = [["admin","password"],["user1","1234"]]
+print("Welcome to the Batty Boys Movie Ticket System")
+print("Please Enter Command (type help for info)")
+
