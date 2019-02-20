d72e0b2e00a35dd606062ae44c2e74ed530dcca0
elton-emmanuel <33668494+elton-emmanuel@users.noreply.github.com>
2019-02-20 15:24:36 -0500

    Login System Working With arrays(No file input)

--- test.py
+++ test.py
@@ -1,5 +1,33 @@
 from array import *
-LoginTemp = [["admin","password"],["user1","1234"]]
+loginTemp = ["admin","user1","1234"]
 print("Welcome to the Batty Boys Movie Ticket System")
-print("Please Enter Command (type help for info)")
+exit = 0
+login = 0
+
+while exit != 1:
+    print("Please Enter Command (type help for info)")
+    command = input()
+    if command =="exit":
+        exit = 1
+    if command == "help":
+        if login == 0:
+            print("login = login to the system \n exit = exit the system")
+        else:
+            print("logout = logout of the system \n buy = buy movie tickets \n sell = sell movie tickets ")
+    if command == "login":
+        if login != 1:
+            print("Enter UserName")
+            username = input()
+            found = 0
+            for name in loginTemp:
+                if name == username:
+                   found = 1
+            if found == 1:
+                print("Logged in Successfully")
+                login = 1
+            else:
+                print("User not found on System")   
+        else:
+            print("Already Logged in")
+
 
